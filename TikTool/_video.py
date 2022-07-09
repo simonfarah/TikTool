0da7f6bd0e07bc1fcc9f53620d7a166899acfc94
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2022-07-09 22:39:22 +0300

    Option to download video in the Video class

--- TikTool/_video.py
+++ TikTool/_video.py
@@ -29,3 +29,25 @@ class Video:
             ).json()
         else:
             self.data = None
+
+    def downloadVideo(self, watermark=True):
+        """
+        Download a public Tiktok video
+
+        @param watermark (optional, default -> True)
+        set the watermark to True to download video with watermark
+        set the watermark to False to download video without watermark
+        """
+
+        # if the data is None which means that the video does not exist, we return False
+        if self.data is None:
+            return False
+
+        download_url = self.data["aweme_details"][0]["video"][
+            "download_addr" if watermark else "play_addr"
+        ]["url_list"][0]
+        download_path = "video.mp4"
+        download(download_url, download_path)
+
+        # return True when the video is downloaded
+        return True
